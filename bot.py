fb2b1872eec20352b14b28891a0f5044743750e3
Antonkulik2001 <antonkulik201@gmail.com>
2024-10-08 19:31:40 +0300

    Визуальные изменение

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import logging
 from dotenv import load_dotenv
 
 from telegram import ForceReply, Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackQueryHandler
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -17,30 +17,43 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Для начала работы напишите команду /start.")
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
     await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Этот создан чтобы запланировать твой будующий отуск! Давай начнем",
-        reply_markup=ForceReply(selective=True),
+        rf"Привет {user.mention_html()}! Этот создан чтобы запланировать твой будующий отуск! Давай начнем"
     )
     keyboard = [
         [
-            InlineKeyboardButton("Option 1", callback_data="1"),
-            InlineKeyboardButton("Option 2", callback_data="2"),
+            InlineKeyboardButton("Запланировать даты отпуска", callback_data="1"),
+            InlineKeyboardButton("Запланировать важные дела", callback_data="2"),
         ],
-        [InlineKeyboardButton("Option 3", callback_data="3")],
+        [InlineKeyboardButton("Запланировать поездку", callback_data="3")],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+    await update.message.reply_text("Пожалуйста выберите действие:", reply_markup=reply_markup)
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+
+    await query.answer()
+
+    await query.edit_message_text(text=f"Selected option: {query.data}")
 
 
 def main() -> None:
-    """Start the bot."""
     application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CallbackQueryHandler(button))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
